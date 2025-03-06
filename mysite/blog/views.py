ddfb95b8ff305674ed36a3caeb50eab6d6912851
IvanTsvetkov-dev <vvvanusha62@mail.ru>
2025-03-06 19:22:30 +0300

    Added Canonical URL for post.Created friendly for search optimization URL-adress posts. Added Pagination in post_list page

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request=request, template_name='blog/post/list.html', context={'posts': posts})
+    page_number = request.GET.get('page', 1) # Извлечение GET параметра. Если параметр отсутствует, то туда записывается стандратное значение 1
+    # Постраничная разбивка 3 постами на страницу
+    paginator = Paginator(posts, 2)
+    try:
+        post = paginator.page(page_number)
+    except EmptyPage:
+        post = paginator.page(paginator.num_pages) # paginator.num_pages возвращает количество страниц. Это означает, что если пользователь введёт в query параметрах page >, чем возможно, то его перенаправит на самую последнюю страницу
+    except PageNotAnInteger:
+        post = paginator.page(1)
+    return render(request=request, template_name='blog/post/list.html', context={'posts': post})
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month = month,
+                             publish__day = day)
     return render(request=request, template_name='blog/post/detail.html', context={'post': post})
     
